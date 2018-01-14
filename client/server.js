c2f9811b2b9f62385e50f486fd4036a5a559cd80
Lars Karbo <larskarbo@hotmail.com>
2018-01-14 20:55:10 +0100

    fix indent

--- client/server.js
+++ client/server.js
@@ -2,49 +2,49 @@ import axios from 'axios'
 
 
 module.exports = {
-	save: (data, title, description, cb) => {
-	  var dataForDatabase = {
-	    metaInfo: {
-	    	title,
-	    	description
-	    },
-	    chartData: {
-	      ...data
-	    }
-	  }
-	  console.log(data)
-	  axios.post('/api/create', {
-	      data: JSON.stringify(dataForDatabase)
-	  })
-	  .then((response) => {
-	    console.log(response)
-	    var chartid = response.data.id
-	    window.history.pushState(response.data, '', '/' + chartid)
-	    cb(chartid)
-	  })
-	},
+  save: (data, title, description, cb) => {
+    var dataForDatabase = {
+      metaInfo: {
+      	title,
+      	description
+      },
+      chartData: {
+        ...data
+      }
+    }
+    console.log(data)
+    axios.post('/api/create', {
+        data: JSON.stringify(dataForDatabase)
+    })
+    .then((response) => {
+      console.log(response)
+      var chartid = response.data.id
+      window.history.pushState(response.data, '', '/' + chartid)
+      cb(chartid)
+    })
+  },
 
-	loadChart: (loading, loadFinish, cb) => {
-	  // first check if fetch is needed
-	  var url = window.location.href
+  loadChart: (loading, loadFinish, cb) => {
+    // first check if fetch is needed
+    var url = window.location.href
 
-	  if (url.split('/')[3].length == 0) {
-	    return cb({})
-	  } else {
-	    var splitted = url.split('/')
-	    var chartid = splitted[splitted.length - 1]
-	  }
-	  console.log(chartid)
-	  loading()
-	  axios.get(`/api/get?chartid=${chartid}`, )
-	  .then(response => {
-	    var data = {
-	      ...response.data,
-	      ...response.data.chartData,
-	    }
-	    loadFinish()
+    if (url.split('/')[3].length == 0) {
+      return cb({})
+    } else {
+      var splitted = url.split('/')
+      var chartid = splitted[splitted.length - 1]
+    }
+    console.log(chartid)
+    loading()
+    axios.get(`/api/get?chartid=${chartid}`, )
+    .then(response => {
+      var data = {
+        ...response.data,
+        ...response.data.chartData,
+      }
+      loadFinish()
 
-	    return cb(data)
-	  })
-	}
+      return cb(data)
+    })
+  }
 }
