535e80333da72a7abeda2e8e5c51477df9d7bf1d
Lars Karbo <mail@larskarbo.no>
2019-10-15 01:25:43 +0200

    Updates

---
+++ precache-manifest.1acf6ba7063832b025054fa563aeb379.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421e1fd47321bc4dc71b3361cd00f26a",
+    "url": "/napchart-website/index.html"
+  },
+  {
+    "revision": "17f199b77f58e38c38fb",
+    "url": "/napchart-website/static/css/main.2233064c.chunk.css"
+  },
+  {
+    "revision": "b68bc686e3a4929f6bf3",
+    "url": "/napchart-website/static/js/2.f08e2d99.chunk.js"
+  },
+  {
+    "revision": "17f199b77f58e38c38fb",
+    "url": "/napchart-website/static/js/main.0243715a.chunk.js"
+  },
+  {
+    "revision": "621236c44eab11553671",
+    "url": "/napchart-website/static/js/runtime-main.75a02e93.js"
+  }
+]);
