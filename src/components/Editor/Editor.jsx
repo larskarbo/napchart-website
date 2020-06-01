b4d8f4456d1117011426f10dcf7e6c28380da1d5
Juanito Taveras <juanito.taveras@gmail.com>
2020-05-31 19:07:41 -0700

    Migrates some elements to tsx

--- src/components/Editor/Editor.jsx
+++ src/components/Editor/Editor.jsx
@@ -1,37 +1,37 @@
-import PopupVoter from './PopupVoter'
+import PopupVoter from "./PopupVoter";
 
-import c from 'classnames'
+import c from "classnames";
 
-import React from 'react'
+import React from "react";
 
-import Header from './Header'
-import BadBrowser from './BadBrowser'
-import ToolBar from './ToolBar'
-import Chart from './Chart'
-import Link from '../common/Link'
+import Header from "./Header";
+import BadBrowser from "./BadBrowser";
+import ToolBar from "./ToolBar";
+import Chart from "./Chart";
+import Link from "../common/Link";
 import { Helmet } from "react-helmet";
 
-import Export from './sections/Export'
-import Info from './sections/Info'
-import Polyphasic from './sections/Polyphasic'
-import Controls from './sections/Controls'
+import Export from "./sections/Export";
+import { Info } from "./sections/Info";
+import Polyphasic from "./sections/Polyphasic";
+import Controls from "./sections/Controls";
 
-import Cookies from 'js-cookie';
-import NotificationSystem from 'react-notification-system'
+import Cookies from "js-cookie";
+import NotificationSystem from "react-notification-system";
 
-import server from '../../server'
+import server from "../../server";
 import { Grommet, Box, Button, Image, Text, Layer } from "grommet";
 const myTheme = {
   global: {
     colors: {
-      brand: '#EA4335'
-    }
-  }
+      brand: "#EA4335",
+    },
+  },
 };
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
-    console.log('7.nov 2019')
+    super(props);
+    console.log("7.nov 2019");
     var chartid = false;
     if (window.location.pathname.length == 6) {
       chartid = window.location.pathname.substring(1);
@@ -41,69 +41,84 @@ export default class App extends React.Component {
       loading: false,
       url: window.location.origin + "/",
       chartid: chartid,
-      title: '',
-      description: '',
+      title: "",
+      description: "",
       currentSection: this.getInitialSection(),
       ampm: this.getAmpm(),
-      showPopup: false
-    }
+      showPopup: false,
+    };
   }
 
   render() {
-
     var sections = [
       {
-        element: <Controls
-          napchart={this.state.napchart}
-          description={this.state.description}
-          changeDescription={this.changeDescription}
-        />,
-        text: 'Chart',
-        title: 'Chart controls'
+        element: (
+          <Controls
+            napchart={this.state.napchart}
+            description={this.state.description}
+            changeDescription={this.changeDescription}
+          />
+        ),
+        text: "Chart",
+        title: "Chart controls",
       },
       {
-        element: <Export
-          url={this.state.url}
-          chartid={this.state.chartid}
-        />,
-        text: 'Share',
-        title: 'Share and export'
+        element: <Export url={this.state.url} chartid={this.state.chartid} />,
+        text: "Share",
+        title: "Share and export",
       },
       {
         element: <Polyphasic napchart={this.state.napchart} />,
-        text: 'Sleep',
-        title: 'Polyphasic Sleep'
+        text: "Sleep",
+        title: "Polyphasic Sleep",
       },
       {
         element: <Info ampm={this.state.ampm} setAmpm={this.setAmpm} />,
-        text: 'About',
-        title: 'About'
-      }
-    ]
-
+        text: "About",
+        title: "About",
+      },
+    ];
 
     return (
       <Grommet theme={myTheme}>
         <div className="Editor">
-          {this.state.showPopup &&
-            <PopupVoter onClose={() => {
-              console.log('closing')
-              this.setState({
-                showPopup: false
-              })
-            }} />
-          }
+          {this.state.showPopup && (
+            <PopupVoter
+              onClose={() => {
+                console.log("closing");
+                this.setState({
+                  showPopup: false,
+                });
+              }}
+            />
+          )}
           <BadBrowser />
-          <NotificationSystem ref={(notificationSystem) => this._notify = notificationSystem} />
+          <NotificationSystem
+            ref={(notificationSystem) => (this._notify = notificationSystem)}
+          />
           <Helmet>
-            {this.state.description.length && <meta name="description" content={this.state.description} />}
-            <meta name="twitter:image" content={`http://thumb.napchart.com:1771/api/getImage?chartid=${this.state.chartid}&width=600&height=600&shape=circle`} />
-            <meta property="og:image" content={`http://thumb.napchart.com:1771/api/getImage?chartid=${this.state.chartid}&width=600&height=600&shape=circle`} />
+            {this.state.description.length && (
+              <meta name="description" content={this.state.description} />
+            )}
+            <meta
+              name="twitter:image"
+              content={`http://thumb.napchart.com:1771/api/getImage?chartid=${this.state.chartid}&width=600&height=600&shape=circle`}
+            />
+            <meta
+              property="og:image"
+              content={`http://thumb.napchart.com:1771/api/getImage?chartid=${this.state.chartid}&width=600&height=600&shape=circle`}
+            />
             <meta property="og:image:width" content="600" />
             <meta property="og:image:height" content="600" />
-            {this.state.title.length && <title>{`${this.state.title} - Napchart`}</title>}
+            {this.state.title.length && (
+              <title>{`${this.state.title} - Napchart`}</title>
+            )}
           </Helmet>
-          <div className={c("grid", { slideSidebarMobile: this.state.slideSidebarMobile })}>
+          <div
+            className={c("grid", {
+              slideSidebarMobile: this.state.slideSidebarMobile,
+            })}
+          >
             <div className="sidebar">
               <Header
                 title={this.state.title}
@@ -113,41 +128,69 @@ export default class App extends React.Component {
                 loading={this.state.loading}
               />
 
-
               <div className="sidebarContent">
                 <div className="sideLane">
                   <div className="up">
-                    {sections.map((section, i) =>
-                      <button onClick={this.changeSection.bind(null, i)} key={i}
-                        className={c("squareBtn", { 'active': (i == this.state.currentSection) })}>
+                    {sections.map((section, i) => (
+                      <button
+                        onClick={this.changeSection.bind(null, i)}
+                        key={i}
+                        className={c("squareBtn", {
+                          active: i == this.state.currentSection,
+                        })}
+                      >
                         {section.text}
                       </button>
-                    )}
+                    ))}
                   </div>
-                  <div className="down">
-                  </div>
+                  <div className="down"></div>
                 </div>
 
                 <div className="otherLane">
-                  <ToolBar napchart={this.state.napchart} title={sections[this.state.currentSection].title} />
+                  <ToolBar
+                    napchart={this.state.napchart}
+                    title={sections[this.state.currentSection].title}
+                  />
                   <div className="currentInfo">
-
                     {sections[this.state.currentSection].element}
-                    <div className="dz" style={{
-                      marginTop: 10,
-                      display: "flex",
-
-                    }}>
-                      <div style={{
-                        width: "50%"
-                      }}>
-                        <h2>Neurotechnology for reduced nighttime awakenings.</h2>
-                        <p><a href="https://drowzee.com/">Drowzee</a> is developing a neural interface to train your brain go deeper into sleep and be less interruptive.</p>
-                        <p><a target="_blank" href="https://drowzee.com/waking-up-middle-of-night/"><strong>Waking up in the middle of the night?</strong></a></p></div>
-                      <div style={{
-                        width: "50%"
-                      }}><img src="/dz.png" /></div>
-
+                    <div
+                      className="dz"
+                      style={{
+                        marginTop: 10,
+                        display: "flex",
+                      }}
+                    >
+                      <div
+                        style={{
+                          width: "50%",
+                        }}
+                      >
+                        <h2>
+                          Neurotechnology for reduced nighttime awakenings.
+                        </h2>
+                        <p>
+                          <a href="https://drowzee.com/">Drowzee</a> is
+                          developing a neural interface to train your brain go
+                          deeper into sleep and be less interruptive.
+                        </p>
+                        <p>
+                          <a
+                            target="_blank"
+                            href="https://drowzee.com/waking-up-middle-of-night/"
+                          >
+                            <strong>
+                              Waking up in the middle of the night?
+                            </strong>
+                          </a>
+                        </p>
+                      </div>
+                      <div
+                        style={{
+                          width: "50%",
+                        }}
+                      >
+                        <img src="/dz.png" />
+                      </div>
                     </div>
                   </div>
                 </div>
@@ -160,113 +203,122 @@ export default class App extends React.Component {
                 onUpdate={this.somethingUpdate}
                 setMetaInfo={this.setMetaInfo}
                 setGlobalNapchart={this.setGlobalNapchart}
-                onLoading={this.loading} onLoadingFinish={this.loadingFinish}
+                onLoading={this.loading}
+                onLoadingFinish={this.loadingFinish}
                 ampm={this.state.ampm}
               />
             </div>
           </div>
 
-          {this.state.slideSidebarMobile &&
-            <button className="button is-light is-large slider left" onClick={this.slideSidebarMobile}>
+          {this.state.slideSidebarMobile && (
+            <button
+              className="button is-light is-large slider left"
+              onClick={this.slideSidebarMobile}
+            >
               →
-          </button>
-          }
-          {!this.state.slideSidebarMobile &&
-            <button className="button is-light is-large slider right" onClick={this.slideSidebarMobile}>
+            </button>
+          )}
+          {!this.state.slideSidebarMobile && (
+            <button
+              className="button is-light is-large slider right"
+              onClick={this.slideSidebarMobile}
+            >
               ←
-          </button>
-          }
+            </button>
+          )}
         </div>
       </Grommet>
-    )
+    );
   }
 
   slideSidebarMobile = () => {
     this.setState({
-      slideSidebarMobile: !this.state.slideSidebarMobile
-    })
-  }
+      slideSidebarMobile: !this.state.slideSidebarMobile,
+    });
+  };
 
   changeSection = (i) => {
     this.setState({
-      currentSection: i
-    })
-  }
+      currentSection: i,
+    });
+  };
 
   setGlobalNapchart = (napchart) => {
     this.setState({
-      napchart: napchart
-    })
-  }
+      napchart: napchart,
+    });
+  };
 
   setMetaInfo = (title, description) => {
-    console.log('title, description: ', title, description);
+    console.log("title, description: ", title, description);
     this.setState({
       title,
-      description
-    })
-  }
+      description,
+    });
+  };
 
   somethingUpdate = (napchart) => {
-    this.forceUpdate()
-  }
+    this.forceUpdate();
+  };
 
   loadingFinish = () => {
     this.setState({
-      loading: false
-    })
-  }
+      loading: false,
+    });
+  };
 
   loading = () => {
     this.setState({
-      loading: true
-    })
-  }
+      loading: true,
+    });
+  };
 
   save = () => {
-    this.loading()
-    var firstTimeSave = !this.props.chartid
-    server.save(this.state.napchart.data, this.state.title,
-      this.state.description, (err, chartid) => {
-        this.loadingFinish()
+    this.loading();
+    var firstTimeSave = !this.props.chartid;
+    server.save(
+      this.state.napchart.data,
+      this.state.title,
+      this.state.description,
+      (err, chartid) => {
+        this.loadingFinish();
         if (err) {
           this._notify.addNotification({
             message: err,
-            level: 'error'
-          })
+            level: "error",
+          });
         } else {
-          this.onSave(chartid)
+          this.onSave(chartid);
         }
-
-      })
-  }
+      }
+    );
+  };
 
   onSave = (chartid) => {
     // refresh (feels better for the user)
-    window.location = '/' + chartid + '?s=1'
-  }
+    window.location = "/" + chartid + "?s=1";
+  };
 
-  changeTitle = event => {
+  changeTitle = (event) => {
     this.setState({
-      title: event.target.value
-    })
-  }
+      title: event.target.value,
+    });
+  };
 
-  changeDescription = event => {
+  changeDescription = (event) => {
     this.setState({
-      description: event.target.value
-    })
-  }
+      description: event.target.value,
+    });
+  };
 
   setNumberOfLanes = (lanes) => {
-    this.state.napchart.setNumberOfLanes(lanes)
-  }
+    this.state.napchart.setNumberOfLanes(lanes);
+  };
 
   getAmpm = () => {
-
-    const cookiePref = Cookies.get('preferAmpm')
+    const cookiePref = Cookies.get("preferAmpm");
     if (cookiePref) {
-      return eval(cookiePref)
+      return eval(cookiePref);
     }
 
     var date = new Date(Date.UTC(2012, 11, 12, 3, 0, 0));
@@ -274,32 +326,31 @@ export default class App extends React.Component {
 
     //apparently toLocaleTimeString() has a bug in Chrome. toString() however returns 12/24 hour formats. If one of two contains AM/PM execute 12 hour coding.
     if (dateString.match(/am|pm/i) || date.toString().match(/am|pm/i)) {
-      return true
+      return true;
+    } else {
+      return false;
     }
-    else {
-      return false
-    }
-  }
+  };
 
   setAmpm = (ampm) => {
-    Cookies.set('preferAmpm', ampm)
+    Cookies.set("preferAmpm", ampm);
 
     this.setState({
-      ampm: ampm
-    })
-  }
+      ampm: ampm,
+    });
+  };
 
   getInitialSection = () => {
     // should always return 0 except when s=1 found in url, because
     // then the user just saved chart and we will show share section instead
 
-    if (window.location.toString().includes('s=1')) {
+    if (window.location.toString().includes("s=1")) {
       // this.maybeVote()
-      return 1
+      return 1;
     }
 
-    return 0
-  }
+    return 0;
+  };
 
   // maybeVote() {
   //   setTimeout(() => {
---
+++ src/components/Editor/sections/Info.tsx
@@ -0,0 +1,92 @@
+import Feedback from "./Feedback";
+
+import React, { FunctionComponent, useState } from "react";
+import server from "../../../server";
+
+type InfoProps = {
+  setAmpm: (any) => void;
+  ampm: any;
+};
+
+export const Info: FunctionComponent<InfoProps> = ({ setAmpm, ampm }) => {
+  const [ampmChanged, setAmpmChanged] = useState(false);
+
+  const changeAmpm = (ampm) => {
+    console.log(ampm);
+    setAmpm(ampm);
+    setAmpmChanged(true);
+  };
+
+  return (
+    <div className="Info">
+      <div className="part">
+        <div className="field">
+          <p>
+            Napchart is a time planning tool that helps you visualize time
+            around a 24 hour clock.
+          </p>
+        </div>
+
+        <div className="field">
+          <p>
+            <strong>Create element:</strong> Click on an empty space on the
+            chart and drag
+          </p>
+        </div>
+        <div className="field">
+          <p>
+            <strong>Delete element:</strong> Set duration to zero or press{" "}
+            <kbd>delete</kbd> or <kbd>⌘-⌫</kbd>
+          </p>
+        </div>
+      </div>
+      <Feedback />
+      <div className="part">
+        <h2 className="title is-6">Contribute</h2>
+        <p className="field">
+          Napchart is open-source and hackable. Check out the projects on GitHub
+          🌟
+        </p>
+        <p className="field">
+          <a
+            target="_blank"
+            href="https://github.com/larskarbo/napchart-website"
+          >
+            <strong>napchart-website</strong>
+          </a>{" "}
+          on GitHub
+        </p>
+        <p className="field">
+          <a target="_blank" href="https://github.com/larskarbo/napchart">
+            <strong>napchart</strong>
+          </a>{" "}
+          on GitHub
+        </p>
+      </div>
+      <div className="part">
+        <label className="label">Time format</label>
+        <div className="control">
+          <label className="radio">
+            <input
+              type="radio"
+              name="answer"
+              onChange={changeAmpm.bind(null, true)}
+              checked={ampm}
+            />
+            <span> AM/PM</span>
+          </label>
+          <label className="radio">
+            <input
+              type="radio"
+              name="answer"
+              onChange={changeAmpm.bind(null, false)}
+              checked={!ampm}
+            />
+            <span> 24 hours</span>
+          </label>
+          {ampmChanged && <p>Refresh to update</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
