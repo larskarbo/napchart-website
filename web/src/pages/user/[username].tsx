e7d2c63ec7b586bc4d02d0e24afd032331615004
Lars Karbo <mail@larskarbo.no>
2022-02-03 15:24:42 -0700

    fix link

--- web/src/pages/user/[username].tsx
+++ web/src/pages/user/[username].tsx
@@ -65,7 +65,7 @@ export default function Profile({}) {
           <ol className="flex items-center space-x-4">
             <li>
               <div className="flex items-center">
-                <a href={'/users'} className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
+                <a className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700">
                   Users
                 </a>
               </div>
